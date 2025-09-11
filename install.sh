234e7279cb29bc44d38f80bee3625554f8a6c672
Brian Tafoya <btafoya@briantafoya.com>
2025-09-11 02:42:21 -0400

    chore: Remove outdated implementation summary, test report, and integration tests

--- install.sh
+++ install.sh
@@ -40,7 +40,7 @@ aarch64 | arm64)
     ;;
 esac
 
-GH_REPO="axllent/mailsandbox"
+GH_REPO="btafoya/mailsandbox"
 INSTALL_PATH="${INSTALL_PATH:-/usr/local/bin}"
 TIMEOUT=90
 # This is used to authenticate with the GitHub API. (Fix the public rate limiting issue)
